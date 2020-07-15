086a377dd1d5382e27a823afedb9d5a572391d59
Tiago Murakami <trmurakami@gmail.com>
2020-07-15 00:38:20 -0300

    Initial

--- queries/final/MontarRegistro-TODO.sql
+++ queries/final/MontarRegistro-TODO.sql
@@ -87,7 +87,7 @@ LEFT JOIN
 (
 	SELECT codigo, STRING_AGG(CONVERT(nvarchar(max), CONCAT( codautor,'--', nome, '--',tipo,'--',qualificador)), ';-;') AS autores
 	FROM (
-		SELECT TOP 2147483647 tbaut.cod_acervo as codigo, tbaut.cod_autor as codautor, aut.tit1 as nome, qua.nome as qualificador, aut.tipo as tipo
+		SELECT TOP 100 PERCENT tbaut.cod_acervo as codigo, tbaut.cod_autor as codautor, aut.tit1 as nome, qua.nome as qualificador, aut.tipo as tipo
 		FROM [bnweb2].[dbo].[tbibxau0] tbaut
 		LEFT JOIN [bnweb2].[dbo].[tbibaut0] aut ON tbaut.cod_autor=aut.cod_autor
 		LEFT JOIN [bnweb2].[dbo].[tbibqua0] qua ON tbaut.cod_qualif=qua.cod_qualif
