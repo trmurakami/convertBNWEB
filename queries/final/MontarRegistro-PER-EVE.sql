9eeafb09c3b104d497e36d2bf90637be9a48ca08
Tiago Murakami <trmurakami@gmail.com>
2020-09-25 09:49:11 -0300

    Initial

--- queries/final/MontarRegistro-PER-EVE.sql
+++ queries/final/MontarRegistro-PER-EVE.sql
@@ -158,7 +158,7 @@ LEFT JOIN
 ON ace.cod_acervo=volumes.codigo
 
 LEFT JOIN (
-	SELECT cod_acervo, STRING_AGG(CONCAT(cod_item, '$a',sigla_unidade,'$b',sigla_unidade, '$d', CONVERT(varchar,dt_inc,23),'$h',TRIM(ISNULL(volume,'')),volume_qta,'$i',patrimonio,cod_old,'$o',classificacao,' ',cutter,' ',complemento,' ',data_pub,' ',edicao,'$t',exemp,'$y','$z',TRIM(str_tp_aqui),'$0',baixado, '$7', REPLACE(REPLACE(REPLACE(empresta,'0','2'),'1','0'),'2','1')), ';-;') as items
+	SELECT cod_acervo, STRING_AGG(CONCAT(cod_item, '$a',sigla_unidade,'$b',sigla_unidade, '$d', CONVERT(varchar,dt_inc,23),'$h',TRIM(ISNULL(volume,'')),volume_qta,'$i',patrimonio,cod_old,'$o',classificacao,'$t',exemp,'$y','$z',TRIM(str_tp_aqui),'$0',baixado, '$7', REPLACE(REPLACE(REPLACE(empresta,'0','2'),'1','0'),'2','1')), ';-;') as items
 	FROM [bnweb2].[dbo].[vbibite0]
 	GROUP BY cod_acervo
 ) item
